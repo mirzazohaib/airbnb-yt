ed68d1fbeb7f8eb8c0e9b7207b8465d7d74214bc
Zohaib <54810131+mirzazohaib@users.noreply.github.com>
2023-02-16 15:53:27 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Airbnb Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
